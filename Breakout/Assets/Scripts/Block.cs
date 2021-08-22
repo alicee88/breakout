c760c88c03a286400bc23958840c5da0aa2bd266
alicee88 <alice.easey@gmail.com>
2021-08-22 15:59:08 +0100

    Create block prefab

--- Breakout/Assets/Scripts/Block.cs
+++ Breakout/Assets/Scripts/Block.cs
@@ -6,12 +6,9 @@ public class Block : MonoBehaviour
 {
     [SerializeField] AudioClip breakSound;
 
-    AudioSource audioSource;
-
     // Start is called before the first frame update
     void Start()
     {
-        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
