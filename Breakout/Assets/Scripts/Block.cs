06d61d461a06df5f6f04e1fd1a7fa718800067d0
alicee88 <alice.easey@gmail.com>
2021-08-25 08:30:00 +0100

    Add particle effects when block is hit

--- Breakout/Assets/Scripts/Block.cs
+++ Breakout/Assets/Scripts/Block.cs
@@ -6,9 +6,14 @@ public class Block : MonoBehaviour
 {
     [SerializeField] AudioClip breakSound;
 
+    ParticleSystem hitBurst;
+
     // Start is called before the first frame update
     void Start()
     {
+        hitBurst = FindObjectOfType<ParticleSystem>();
+        var main = hitBurst.main;
+        main.startColor = GetComponent<SpriteRenderer>().color;
     }
 
     // Update is called once per frame
@@ -19,6 +24,9 @@ public class Block : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
         AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
+        ParticleSystem burstInstance = Instantiate(hitBurst, transform.position, transform.rotation);
+        burstInstance.Play();
+       // Destroy(burstInstance);
         Destroy(gameObject);
     }
 }
